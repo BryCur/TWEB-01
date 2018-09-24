5fe8604d04cb2125bf358dc424f5e1e95d765d46
Unknown <bryan.curchod@gmail.com>
2018-09-24 13:59:38 +0200

    finis

--- js/script.js
+++ js/script.js
@@ -10,9 +10,19 @@ btnShowLess.onclick = function (){
         content[0].style.height = "auto";
     } else {
         btnShowLess.innerHTML = "SHOW MORE";
-        content[0].style.height="300px";
+        content[0].style.height="150px";
     }
 
     textDisplayed = !textDisplayed;
     
 }
+
+
+var svg = document.getElementsByClassName("svgClass");
+
+svg.forEach( 
+    function(currentValue, currentIndex, listObj) { 
+      console.log(currentValue + ', ' + currentIndex + ', ' + this); 
+    },
+    'myThisArg'
+  );
